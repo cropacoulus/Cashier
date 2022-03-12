d0255becddaf9a7ca06de12302aa6db12d601f7d
cropacoulus <desmar217@gmail.com>
2022-03-12 17:06:28 +0700

    Store Products

--- components/Product.vue
+++ components/Product.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="product">
+  <v-row id="product">
     <v-row align="center">
       <v-col cols="10">
         <v-autocomplete
@@ -58,10 +58,12 @@
         </v-card>
       </v-col>
     </v-row>
-  </section>
+  </v-row>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default ({
   data() {
     return {
@@ -71,23 +73,6 @@ export default ({
           { id: 1, title: 'Smartphone', },
           { id: 2, title: 'Camera', },
           { id: 3, title: 'Televisi', },
-      ],
-      products: [
-        { id: 1, title: 'Asus Zenfone', thumbnail: 'asus-zenfone.png', price: 2000000, categoryId: 1 },
-        { id: 2, title: 'Canon Eos 700d', thumbnail: 'canon-eos-700d.png', price: 4300000, categoryId: 2 },
-        { id: 3, title: 'Canon Eos 750d', thumbnail: 'canon-eos-750d.png', price: 5400000, categoryId: 2 },
-        { id: 4, title: 'Iphone 6 Silver', thumbnail: 'iphone-6-silver.png', price: 2500000, categoryId: 1 },
-        { id: 5, title: 'Lenovo A7000', thumbnail: 'Lenovo-A7000.png', price: 1500000, categoryId: 1 },
-        { id: 6, title: 'Lg Led Tv 32LF550A', thumbnail: 'lg-32-led-tv-32LF550A.png', price: 2500000, categoryId: 3 },
-        { id: 7, title: 'Lg Led Tv 32LF520A', thumbnail: 'lg-led-tv32-32LF520A.png', price: 2700000, categoryId: 3 },
-        { id: 8, title: 'Mi 4i', thumbnail: 'mi-4i.png', price: 1800000, categoryId: 1 },
-        { id: 9, title: 'Nikon D3200', thumbnail: 'nikon-d3200.png', price: 4500000, categoryId: 2 },
-        { id: 10, title: 'Nikon D5200', thumbnail: 'nikon-d5200.png', price: 6500000, categoryId: 2 },
-        { id: 11, title: 'Galaxy A3', thumbnail: 'samsung-galaxy-A3.png', price: 3000000, categoryId: 1 },
-        { id: 12, title: 'Galaxy A8', thumbnail: 'samsung-galaxy-A8.png', price: 4100000, categoryId: 1 },
-        { id: 13, title: 'Galaxy Grand Prime', thumbnail: 'samsung-galaxy-grand-prime.png', price: 2100000, categoryId: 1 },
-        { id: 14, title: 'Galaxy Note 3', thumbnail: 'samsung-galaxy-note-3.png', price: 4100000, categoryId: 1 },
-        { id: 15, title: 'Sharp Led TV 32LE265i', thumbnail: 'sharp-32-led-32LE265i.png', price: 2300000, categoryId: 3 },
       ],
       search: null,
       isLoading: false,
@@ -109,7 +94,10 @@ export default ({
         return this.products.filter(s => s.title == this.selectedSearch.title)
       }
       return this.products
-    }
+    },
+    ...mapState('products', {
+      products: 'products'
+    })
   },
   watch: {
     search(val){
